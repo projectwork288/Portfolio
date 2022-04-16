1ac46ad5037fbd335ef3ec65d0b04a588ae6049e
Arthur Green <jamie.green@gmail.com>
2022-04-16 09:30:35 -0500

    initial commit for password generator

---
+++ assets/js/javascript.js
@@ -0,0 +1,12 @@
+   // <buttonGen onclick="getElementById('myValue').innerHTML=Date()">Your Password is ... </button>
+   // document.getElementById("myValue").innerHTML = Math.floor(Math.random() * 10000000);
+
+  //  console.log(myValue);
+<p id="demo"></p>
+<script>
+    object.addEventListener("click", myScript);
+    object.onclick = function(){document.getElementById("myValue").innerHTML = Math.floor(Math.random() * 10000000)};
+</script>
+console.log(myValue);
+
+
